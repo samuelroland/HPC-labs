47d07755404423994b59a6d5bcebcb22429abe4f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-02-21 00:58:55 +0100

    Optimize CMakelists with colors

--- lab01/codec.h
+++ lab01/codec.h
@@ -2,6 +2,9 @@
 #ifndef CODEC_H
 #define CODEC_H
 
+#define LINES_FREQ = {697, 770, 852, 941};
+#define COLUMNS_FREQ = {1209, 1336, 1477};
+
 // DTMF encoding from given text and store results in audio_result
 // It doesn't free text but allocates audio_result to the correct size
 void dtmf_encode(const char *text, float *audio_result);
