9f044f8c917a9d86d1730e9e3d7f0cd50194caa0
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-03-25 23:11:06 +0100

    Refactor xmake.lua with add_packages() global

--- lab01/code/xmake.lua
+++ lab01/code/xmake.lua
@@ -1,15 +1,14 @@
 add_rules("mode.debug", "mode.release")
-add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "build" })
 
+-- Global settings for all targets
 add_requires("libsndfile", "fftw")
-
-
 add_files("audio.c", "encoder.c", "decoder.c", "file.c", "fft.c")
+add_packages("libsndfile", "fftw")
 
+-- Target definitions
 target("sandbox")
-    add_files("sandbox.c")
-    add_packages("libsndfile", "fftw")
+add_files("sandbox.c")
 
 target("dtmf_encdec")
-    add_files("main.c")
-    add_packages("libsndfile", "fftw")
+add_files("main.c")
