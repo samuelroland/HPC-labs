4e1d08c799bf92612909ea9306ef41583cd6c1fc
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-04-01 00:05:04 +0200

    Likwid xmake.lua

--- lab02/code/xmake.lua
+++ lab02/code/xmake.lua
@@ -7,6 +7,14 @@ add_files("audio.c", "encoder.c", "decoder.c", "file.c", "fft.c")
 add_packages("libsndfile", "fftw")
 set_policy("check.target_package_licenses", false)
 
+-- Necessary for likwid
+add_links("likwid")
+add_defines("LIKWID_PERFMON")
+
+set_warnings("all")
+set_optimize("none") -- -O0
+add_cflags("-g", "-fno-inline")
+
 -- Target definitions
 target("sandbox")
 add_files("sandbox.c")
