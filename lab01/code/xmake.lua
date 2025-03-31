08e619451d816821c318c2d792b4a3330ab411f9
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-03-31 23:01:59 +0200

    just save unused xmake.lua for lab1

--- lab01/code/xmake.lua
+++ lab01/code/xmake.lua
@@ -5,10 +5,16 @@ add_rules("plugin.compile_commands.autoupdate", { outputdir = "build" })
 add_requires("libsndfile", "fftw")
 add_files("audio.c", "encoder.c", "decoder.c", "file.c", "fft.c")
 add_packages("libsndfile", "fftw")
+set_policy("check.target_package_licenses", false)
 
 -- Target definitions
 target("sandbox")
 add_files("sandbox.c")
 
-target("dtmf_encdec")
+target("dtmf_encdec-goertzel")
+add_defines("DECODER_VARIANT=1")
+add_files("main.c")
+
+target("dtmf_encdec-fft")
+add_defines("DECODER_VARIANT=2")
 add_files("main.c")
