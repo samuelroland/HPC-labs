2e5d06bbadd4d6c26c36458d5d079c8bd5188c4d
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-05-28 01:34:57 +0200

    Add main.c and cmakelists.txt for lab06

---
+++ lab06/code/main.c
@@ -0,0 +1,83 @@
+// Implementation of the float party, measuring the energy used for 2 implementations
+// Just loading a big buffer float values with value 1 and doing the sum of these values
+// This code is made of 2 implementation, one basic and a second one with SIMD
+#include <assert.h>
+#include <immintrin.h>
+#include <stdlib.h>
+
+// #define CODE_VERSION 2// 1 is normal, 2 is SIMD
+#if CODE_VERSION == 1
+int benchmark(int size) {
+    float *bigBuffer = malloc(size * sizeof(float));
+    if (!bigBuffer) return 0;
+
+    for (int i = 0; i < size; i++) {
+        bigBuffer[i] = 1;
+    }
+
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += bigBuffer[i];
+    }
+    free(bigBuffer);
+    return sum;// it will convert to int, as we want a round value to compare with that's good
+}
+#endif
+
+#if CODE_VERSION == 2
+// This SIMD variant is using an accumulator initialized to zero, to sum groups of 8 float together.
+// At the end, we just sum the 8 float and this gives us the final sum
+int benchmark(int size) {
+    float *bigBuffer = malloc(size * sizeof(float));
+    if (!bigBuffer) return 0;
+    __m256 accumulator;
+    _mm256_set1_ps(0);
+    int incr = 8;// 8 float per 256 bits registers
+    int halfincr = 4;
+    int partialMaximum = size - (size % incr);
+
+    float *ptr = bigBuffer;
+    float sum = 0;
+    for (int i = 0; i < partialMaximum; i++) {
+        __m256 current8floats = _mm256_loadu2_m128(ptr, ptr + halfincr);
+        accumulator = _mm256_add_ps(current8floats, accumulator);
+    }
+
+    // Sum values into the accumulator
+    float accFloats[incr];
+    _mm256_store_ps(accFloats, accumulator);
+    for (int i = 0; i < incr; i++) {
+        sum += accFloats[i];
+    }
+
+    // Manage the rest
+    for (int i = partialMaximum; i < size; i++) {
+        sum += bigBuffer[i];
+    }
+
+    free(bigBuffer);
+    return sum;// it will convert to int, as we want a round value to compare with that's good
+}
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int MB_size;
+    if (argc < 2) {
+        printf("Taking default value for size of float buffer\n");
+        MB_size = 10000000;// 10millions of float = 40millions of bytes
+    } else {
+        MB_size = atoi(argv[1]);
+    }
+
+    printf("Starting benchmark with size = %d\n", MB_size);
+    int result = benchmark(MB_size);
+    if (result == MB_size) {
+        printf("Result is correct\n");
+    } else {
+        printf("Error: sum should be %d but got %d\n", MB_size, result);
+    }
+    printf("Benchmark done\n");
+}
